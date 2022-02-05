b801f7e251ad5579b15923b75d88a71acca9dd5c
Andrii Chernyshov <andruha46@gmail.com>
2022-02-05 15:15:32 +0200

    Created 3 buttons in activity Created 3 layouts task 1/2/3 with homework 1 elements.

---
+++ app/src/main/java/com/example/androidlessonhm1/task2.java
@@ -0,0 +1,17 @@
+package com.example.androidlessonhm1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class task2 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task2);
+    }
+
+}
+
+
---
+++ app/src/main/java/com/example/androidlessonhm1/task3.java
@@ -0,0 +1,17 @@
+package com.example.androidlessonhm1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class task3 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task3);
+    }
+
+}
+
+
